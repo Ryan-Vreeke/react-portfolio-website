d66a04b7c168c564b0a58e74fd1c52711f58381f
Ryan-Vreeke <rvreeke1234@gmail.com>
2022-07-29 10:12:12 -0600

    added header components and nav components

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BsGithub} from 'react-icons/bs'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://github.com/Ryan-Vreeke" target="_blank"><BsGithub/></a>
+        <a href="https://github.com/Ryan-Vreeke" target="_blank"><BsGithub/></a>
+        <a href="https://github.com/Ryan-Vreeke" target="_blank"><BsGithub/></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
